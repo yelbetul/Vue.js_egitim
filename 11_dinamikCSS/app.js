8d8edc4a0ff96046d3c896382b9cdadb69503b72
Betül YEL <yellbetul@gmail.com>
2024-08-06 15:15:33 +0300

    dinamik CSS işlendi.

--- 11_dinamikCSS/app.js
+++ 11_dinamikCSS/app.js
@@ -3,13 +3,20 @@ const{ createApp } = Vue
 createApp({
     data(){
         return {
-            aktiflik: true,
+            aktiflik : false,
+            extra : false
+
         }
     },
     methods: {
         
     },
     computed: {
-        
+        degisikligiAlgila : function(){
+            return {
+                aktif : this.aktiflik,
+                extra: this.extra
+            }
+        }
     }
 }).mount("#app");
