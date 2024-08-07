bdabc707d9e30d01f0faeb0b2abbf11dc00350af
Betül YEL <yellbetul@gmail.com>
2024-08-07 14:02:18 +0300

    icicebilesenler devam

---
+++ 21_iciceBilesen/vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <app-header></app-header>
+  <app-article></app-article>
+  <app-footer></app-footer>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Article from './components/article.vue';
+import Footer from './components/footer.vue'
+
+
+export default{
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-article": Article
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style scoped>
+h1{
+  color: red;
+}
+</style>
