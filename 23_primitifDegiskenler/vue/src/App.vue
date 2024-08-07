b957b8ec2e210a5754b2350f0aa03a0890c3c08d
Betül YEL <yellbetul@gmail.com>
2024-08-07 14:56:50 +0300

    Primitif Değişkenler

---
+++ 23_primitifDegiskenler/vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <app-header v-bind:title="title"></app-header>
+  <app-article v-bind:users="users"></app-article>
+  <app-footer v-bind:title="title"></app-footer>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Article from './components/article.vue';
+import Footer from './components/footer.vue'
+
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-article": Article
+  },
+  data() {
+    return {
+      title: "yelbetul",
+      users: [
+        { name: "Gökhun", spec: "Geliştirici", show: false },
+        { name: "Elif", spec: "Dijital Pazarlama Uzmanı", show: false },
+        { name: "Hasan", spec: "Full Stack Developer", show: false },
+        { name: "Berkay", spec: "Back End Developer", show: false },
+        { name: "Yaren", spec: "C# Geliştirici", show: false },
+        { name: "Betül", spec: "Front End Developer - Veri Bilimi Mentörü", show: false },
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ 23_primitifDegiskenler/vue/src/components/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1 v-on:click="changeTitle">{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props:{
+    title:{
+      type:String,
+      required: true
+    }
+  },
+  methods:{
+    changeTitle:function(){
+      this.title = "Değişen Başlık"
+    }
+  },
+  data() {
+    return {
+        
+    }
+  }
+}
+</script>
+<style scoped>
+header {
+  background: lightgreen;
+  padding: 10px;
+}
+
+h1 {
+  color: #222;
+  text-align: center;
+}
+</style>
