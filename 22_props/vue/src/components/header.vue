7bc915a04c4edc1df1cd58035637f1e454bc78c3
Betül YEL <yellbetul@gmail.com>
2024-08-07 14:32:08 +0300

    props

---
+++ 22_props/vue/src/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props:{
+    title:{
+      type:String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      // title:"Bu sayfamızın Başlığı"
+
+    }
+  }
+}
+</script>
+<style scoped>
+header {
+  background: lightgreen;
+  padding: 10px;
+}
+
+h1 {
+  color: #222;
+  text-align: center;
+}
+</style>
