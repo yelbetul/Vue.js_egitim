4562a915b6bcdbfca273c06218f17f372807ecaa
Betül YEL <yellbetul@gmail.com>
2024-08-07 16:40:53 +0300

    Slots

---
+++ 26_slots/vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Slotlar</h1>
+  <alt-bilesen>
+
+    <template #baslik>
+      <h2>{{title}}</h2>
+    </template>
+    <template #icerik>
+      <p>Bu alt bileşene gönderilecek içeriktir.</p>
+    </template>
+    <template #baslik2>
+      <h2>Bu alt Bileşene Gönderilecek Başlıktır2.</h2>
+    </template>
+    <template #icerik2>
+      <p>Bu alt bileşene gönderilecek içeriktir.2</p>
+    </template>
+
+  </alt-bilesen>
+</template>
+<script>
+import altBilesen from './components/alt-bilesen.vue';
+export default {
+  components: {
+    "alt-bilesen": altBilesen,
+  },
+  data() {
+    return {
+      title:"merhaba ben alt bileşene gönderilecek basligim"
+    }
+  }
+}
+</script>
---
+++ 26_slots/vue/src/components/alt-bilesen.vue
@@ -0,0 +1,8 @@
+<template>
+    <slot name="baslik"></slot>
+    <slot name="baslik2"></slot>
+    <hr>
+    <slot name="icerik"></slot>
+    <slot name="icerik2"></slot>
+    
+</template>
