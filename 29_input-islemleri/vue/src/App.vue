d73bbd0de2a49a040c92e24955494556d0a8eff9
Betül YEL <yellbetul@gmail.com>
2024-08-07 18:28:02 +0300

    v-model işlendi

---
+++ 29_input-islemleri/vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<add-blog></add-blog>
+</template>
+
+<script>
+import addBlog from './components/add-blog.vue';
+export default{
+  components:{
+    "add-blog": addBlog
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
