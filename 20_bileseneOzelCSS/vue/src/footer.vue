9f57d1943e39fd97227a3a5f2e8019fcf0b6f375
Betül YEL <yellbetul@gmail.com>
2024-08-07 13:30:34 +0300

    scoped

---
+++ 20_bileseneOzelCSS/vue/src/footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <p>{{ copyright }} - {{ year }}</p>
+    </ul>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+        copyright: "Tüm Hakları Saklıdır. (C)",
+        year: 2001
+    }
+  }
+}
+</script>
---
+++ 20_bileseneOzelCSS/vue/src/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Bu İç Component'in Başlığı</h1>
+    <ul>
+        <li v-for="eleman in liste">{{ eleman }}</li>
+    </ul>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      liste: ["Anasayfa","Hakkımızda","Blog","İletişim"]
+    }
+  }
+}
+</script>
+<style scoped>
+h1{
+  color: green;
+}
+</style>
