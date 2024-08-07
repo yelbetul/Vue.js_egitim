56edfc0803e2e6a18943de5db86be3573201fa9d
Betül YEL <yellbetul@gmail.com>
2024-08-07 15:45:01 +0300

    Lifecycle Hooks

---
+++ 25_lifecycleHooks/vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Lifecycle Hooks {{ title }}</h1>
+  <button v-on:click="basligiGuncelle"> Başlığı Güncelle</button>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      title: "deneme başlığı"
+    }
+  },
+  methods:{
+    basligiGuncelle(){
+      this.title = "Başlığı Güncelledim."
+    }
+  },
+  beforeCreate(){
+    alert("create edilmeden önce")
+  },
+  created(){
+    alert("create edildi.")
+  },
+  beforeMount(){
+    alert("mount edilmeden önceki işlemler")
+  },
+  mounted(){
+    alert("mount edildi.")
+  },
+  beforeUpdate(){
+    alert("Update etmeden önce son sözleri olan burda söylesin:)")
+  },
+  updated(){
+    alert("iş işten geçti update çoktan ettim")
+  }
+}
+</script>
