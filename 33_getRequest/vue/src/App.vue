2b8cb51aa7b474c0603f667a35ccccb42cf10a3b
Betül YEL <yellbetul@gmail.com>
2024-08-08 14:15:04 +0300

    GET Request

---
+++ 33_getRequest/vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <add-blog></add-blog> -->
+  <show-blog></show-blog>
+</template>
+
+<script>
+import addBlog from './components/add-blog.vue';
+import showsBlogs from './components/shows-blogs.vue';
+export default {
+  components: {
+    "add-blog": addBlog,
+    "show-blog": showsBlogs
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
